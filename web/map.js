4a86022332640e137eff835965ceaa42f4ae0de1
Shana Friedman <friedman.shana@gmail.com>
2015-05-01 10:36:38 -0400

    fix bugs

--- web/map.js
+++ web/map.js
@@ -1,9 +1,11 @@
-//thurs:
 /*
-
 essential: 
--fix text issue
--out of bounds bug
+-text issue on graphs
+-
+
+-add all directors
+
+-fix font/design
 
 -make box scroll down
 
@@ -11,12 +13,6 @@ add go back to all button
 
 -fix corner cases
 -mark which country was selected
-
-(3. button backgrounds - make buttons pop / collapse css button)
-
-5. Add show all button to go back to start state / dateslider
-
-if time, general highlight box to click on graph and display films and highlight on mouseover / zooming
 
 intro/scroll down and intro disappears - how to do this
 definitely write intro with some data analysis and observations about countries
@@ -32,8 +28,8 @@ no map/no multiple countries
 count/history of violence
 
 films over time
-
 */
+
 var imdb_data;
 var GLOBAL_COUNTRY = "all";
 var colors = ['#0000b4','#0082ca','#0094ff','#0d4bcf','#0066AE','#074285','#00187B','#285964','#405F83','#416545','#4D7069','#6E9985','#7EBC89','#0283AF','#79BCBF','#99C19E'];
@@ -80,7 +76,7 @@ function set_location(country) {
 		var y_margin = 225; //220
 	}
 
-    for(var i=0;i<imdb_data.length;i++){
+    for(var i=1;i<imdb_data.length;i++){
 		if (imdb_data[i].image[0]) { 
 			if (country == "all") {
 				imdb_data[i].x = start_x + (x_margin * x_space) - 10;
@@ -99,9 +95,12 @@ function set_location(country) {
 						x_space = 0;
 						y_space++;
 					}
+				} else {
+					imdb_data[i].x = -1000;
+					imdb_data[i].y = -1000;
 				}
 			} else {
-					if (imdb_data[i].country.indexOf(country) > -1) {
+				if (imdb_data[i].country.indexOf(country) > -1) {
 					imdb_data[i].x = start_x + (x_margin * x_space) - 30;
 					imdb_data[i].y = start_y + (y_margin * y_space);
 					x_space++;
@@ -109,8 +108,11 @@ function set_location(country) {
 						x_space = 0;
 						y_space++;
 					}
+				} else {
+					imdb_data[i].x = -1000;
+					imdb_data[i].y = -1000;
 				}
-			} 	//need else if for USA
+			}
 		}
 	}
 
@@ -118,9 +120,9 @@ function set_location(country) {
 }
 
 function display_posters(country) {
-	imdb_data = set_location(country);
+	d3.select("#posters").remove();
 
- 	d3.select("#posters").remove();
+	imdb_data = set_location(country);
 
 	var w = window,
 	    d = document,
@@ -138,8 +140,6 @@ function display_posters(country) {
 		.attr("width", x)
 		.attr("height", 4000);
 		//.attr("perspective", "800px");
-
-	//on click checking global bool here
 
     var imgs = d3.select("#posters").selectAll("image")
     	.data(imdb_data)
@@ -172,24 +172,34 @@ function display_posters(country) {
     			return 225;
     		}
     	})
-    	.on("mouseover", function (d) {	//need to fix for edge cases
-    		d3.select("#title").text("Title: " + d.title[0])
-    		d3.select("#year").text("Year: " + d.year[0])
-    		d3.select("#director").text("Director: " + d.director[0])
+    	.on("mouseover", function (d) {
+    		// console.log(d);
+    		// if (d.title == "Innocent Voices") {
+    		// 	console.log(d.image);
+    		// }
+    		// console.log(d.title[0] + d.image);
     		var sel = d3.select(this);
     		this.parentNode.appendChild(this);
+    		if(d.title[0] != "Ying xiong lie nu") {
+    			d3.select("#title").text("Title: " + d.title[0])
+    			d3.select("#year").text("Year: " + d.year[0])
+    			d3.select("#director").text("Director: " + d.director[0])
+    		}
+
     		if (country == "all" || country == "USA") {
-    			//sel.attr("width", 175).attr("height", 175).attr("x", d.x - 30).attr("y", d.y - 30); //.transition(); //175, 30 looks good - 300 for zoom
+    			//.transition(); //175, 30
+    			sel.attr("width", 100).attr("height", 100).attr("x", d.x - 25).attr("y", d.y - 25); 
     		} else {
+    			if (d.x >= 10 && d.y >= 10)
     			sel.attr("width", 235).attr("height", 235).attr("x", d.x - 3).attr("y", d.y - 3)
     		}
     	})
     	.on("mouseout", function(d) {
-    		// d3.select("#title").text("Title: ")
-    		// d3.select("#year").text("Year: ")
-    		// d3.select("#director").text("Director: ")
+    		d3.select("#title").text("Title: ")
+    		d3.select("#year").text("Year: ")
+    		d3.select("#director").text("Director: ")
     		if (country == "all" || country == "USA") {
-    	  		//d3.select(this).attr("width", 50).attr("height", 50).attr("x", d.x).attr("y", d.y);
+    	  		d3.select(this).attr("width", 50).attr("height", 50).attr("x", d.x).attr("y", d.y);
     	  	} else {
     	  		d3.select(this).attr("width", 225).attr("height", 225).attr("x", d.x).attr("y", d.y);
     	  	}
@@ -203,10 +213,6 @@ function build_buttons(country_data) {
 		});
 }
 
-//fix font and colors - sizes a little smaller to fit all of asia
-//onclick to each bar graph element which calls films render function
-//start out with all films displaying
-//button to go back to all films
 function display_countries(country_data, region) {
 	var num_countries = regions_with_countries[region].length;
 
@@ -222,7 +228,7 @@ function display_countries(country_data, region) {
 
 	var xscale = d3.scale.linear()
 					.domain([0,max])
-					.range([0,300]); //domain is input, range is output
+					.range([0,100]); //domain is input, range is output
 
 	var yscale = d3.scale.linear()
 					.domain([0,num_countries])
@@ -245,7 +251,7 @@ function display_countries(country_data, region) {
 				.enter()
 				.append('rect')
 				.attr('height', 20)
-				.attr({'x':100, 'y':function(d,i) {
+				.attr({'x':140, 'y':function(d,i) {
 					if (regions_with_countries[region].indexOf(d) > -1) {
 						return yscale(regions_with_countries[region].indexOf(d)) + 30 + regions_with_countries[region].indexOf(d)*30; 
 					}
@@ -265,15 +271,12 @@ function display_countries(country_data, region) {
 				.on("mouseout", function(d){
 					return tooltip.style("visibility", "hidden");
 				})
-				// .on("mouseover", function(d) {
-				// 	//console.log(country_data[d]);
-				// })
 				.on("click", function(d) {
 					display_posters(d);
 				}); 
 
 	var y_pos;
-	var transitext = d3.select('#bars')		//if too long, put on next line - get length of string js?
+	var transitext = d3.select('#bars')
 						.selectAll('text')
 						.data(d3.keys(country_data))
 						.enter()
@@ -289,17 +292,26 @@ function display_countries(country_data, region) {
 						}})
 						.text(function(d) { 
 							if (regions_with_countries[region].indexOf(d) > -1) {
-								return d; 
+								if (d == "Bosnia and Herzegovina") {
+									return "Bosnia";
+								} else if (d == "Federal Republic of Yugoslavia") {
+									return "F.R. of Yugoslavia";
+								} else if (d == "Republic of Macedonia") {
+									return "Rep. of Macedonia";
+								} else if (d == "Serbia and Montenegro") {
+									return "Serbia";
+								}
+								else {
+									return d; 
+								}
 							}
 						}).style({'fill':'black','font-size':'11px', 'font-family':'sans-serif', 'font-weight':'bold'});
 
 }
 
-
-//put these inside another function, on ready state etc
 $.getJSON( "../data/data.json", function(json) {
 	imdb_data = json;
- 	display_posters("Mexico");
+ 	display_posters("Cuba");
  	d3.select(".selection-box").style("background", "grey");
  	d3.selectAll("button").style("visibility", "visible");
  	d3.select("#title").style("visibility", "visible");
